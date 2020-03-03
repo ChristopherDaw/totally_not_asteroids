d59663157715848076ee3ea01e60f2d9189de20b
chittic3 <chittic3@wwu.edu>
2020-03-02 17:09:50 -0800

    some thoughts and notes

--- game/gameScene.js
+++ game/gameScene.js
@@ -1,5 +1,6 @@
 var Game = function(canvas, gl) {
     this.cameraAngle = 0;
+    //If we can only init this with a set num of objects we need to hide enemies off screen and cycle them in and out
     this.cubeMesh = new ShadedTriangleMesh(gl, CubePositions, CubeNormals, CubeIndices, LambertVertexSource, LambertFragmentSource);
     
     gl.enable(gl.DEPTH_TEST);
@@ -13,9 +14,18 @@ Game.prototype.render = function(canvas, gl, w, h) {
     
     var view = SimpleMatrix.rotate(this.cameraAngle, 1, 0, 0).multiply(
         SimpleMatrix.translate(0, 0, 6));
-        
+    
+    //We need to add event handeling for these transformations to operate respective to input 
     var rotation = SimpleMatrix.rotate(Date.now()/25, 0, 1, 0);
     var cubeModel = SimpleMatrix.translate(0, 0, 0).multiply(rotation);
+
+    //We also need to use a timer (like date about) and as time proceeds transform our enemy objects
+
+    /*End of TODO before milestone*/
+
+    //Create collision detection that we check every frame here
+
+    //Implement game state that changes this whole render function depending on state
 
     this.cubeMesh.render(gl, cubeModel, view, projection);
 }
